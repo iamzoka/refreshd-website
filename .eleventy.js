beaa7ba1261fdb283acd6215811e385580963b53
Zoran Zlokapa <z.zoran@gmail.com>
2020-06-29 19:00:08 +0200

    Started refactoring 11ty; Removed unneeded dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,95 +1,87 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { DateTime } = require('luxon');
+const fs = require('fs');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy');
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
 
   // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
       return array.slice(n);
     }
 
     return array.slice(0, n);
   });
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('css');
 
   /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let markdownItAttrs = require("markdown-it-attrs");
-  
+  let markdownIt = require('markdown-it');
+  let markdownItAnchor = require('markdown-it-anchor');
+  let markdownItAttrs = require('markdown-it-attrs');
+
   let options = {
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   };
   let opts = {
     permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
   };
 
   let markdownItOptions = markdownIt(options).use(markdownItAnchor, markdownItAttrs, opts);
 
-  eleventyConfig.setLibrary("md", markdownItOptions);
+  eleventyConfig.setLibrary('md', markdownItOptions);
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
+      ready: function (err, browserSync) {
         const content_404 = fs.readFileSync('_site/404.html');
 
-        browserSync.addMiddleware("*", (req, res) => {
+        browserSync.addMiddleware('*', (req, res) => {
           // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    templateFormats: ['md', 'njk', 'html'],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
     // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
     // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
+    pathPrefix: '/',
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
     passthroughFileCopy: true,
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      input: 'src',
+    },
   };
 };
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Refresh'd
 
 My personal website built with 11ty.
+
+## 100 Days of code refactor
+
+I've started refactoring 11ty setup on 29-6-2020 as part of my #100daysofcode journey.
+
+When I first deployed this version of the site, I used some starter kit just to get me going. Now, my idea is to
+get rid of everyting non-essential and add just what I want, so I cleaned out `dependencies` from `package.json`.
+It appears 11ty can run without it, who new!
+
+Then, I moved all templates and assets to `src/` for cleaner file tree. Also, started removing stuff from `.eleventy.js`
+I don't need.
+
+One thing I wanted to add was Parcel bundler for bundling styles and JavaScript.
+
+Honestly, I thought I'll have to delete everything and start from scratch, but it worked on the first try.
+
+## To do:
+
+[x] Clean out project repo
+[x] Implement bundling with Parcel
+[ ] Remove everything non-essential
+[ ] Clean out `eleventy.js`
+[ ] Write an article about this refactor
